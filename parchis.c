38a6ffa592774354be3e5348a198ab3cdd76c979
Edgar <edgardiazortiz@gmail.com>
2021-08-19 21:50:41 -0500

    entrada

---
+++ parchis.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void rellenarady(int ady[17][12])
+{
+
+	for(int k=0;k<17;k++)
+	{
+
+		for(int j=0;j<12;j++)
+			ady[k][j]=0;
+
+	}
+
+		
+		ady[0][1]=1;
+		ady[1][2]=1;
+		ady[2][3]=1;
+
+		ady[4][5]=1;
+		ady[5][6]=1;
+		ady[6][7]=1;
+
+		ady[8][9]=1;
+		ady[9][10]=1;
+		ady[10][11]=1;
+
+		ady[0][4]=1;
+		ady[1][5]=1;
+		ady[2][6]=1;
+		ady[3][7]=1;
+
+		ady[4][8]=1;
+		ady[5][9]=1;
+		ady[6][10]=1;
+		ady[7][11]=1;
+
+}
+
+void imprimirtab(int ady[17][12])
+{
+
+	int conady=0,conadyv=0;
+	for(int k=0;k<3;k++)
+	{
+		for(int j=0;j<4;j++)
+		{	
+
+			
+			printf("*");
+			
+			if(ady[conady][conady+1]==2)
+				printf("---");
+			else
+				printf("   ");
+
+			conady++;
+		}
+		printf("\n");
+	
+		for(int j=0;j<4;j++)
+		{
+
+			if(ady[conadyv][conadyv+4]==2)
+				printf("|   ");
+			else
+				printf("   ");
+
+			conadyv++;
+
+		}
+		printf("\n");
+
+
+	}
+
+}
+
+int main()
+{
+
+	int tablero[4][3]={0,1,2,3,
+					   4,5,6,7,
+					   8,9,10,11};
+	int ady[17][12];
+	
+	rellenarady(ady);
+	imprimirtab(ady);
+
+	int elecc[2];
+
+
+	printf("Ingrese los puntos a juntar: ");
+	scanf("%d",&elecc[0]);
+	scanf("%d",&elecc[1]);
+	
+	//bool ban=false;
+
+	ady[elecc[0]][elecc[1]]++;
+	
+	
+
+	for(int k=0;k<17;k++)
+	{
+
+		for(int j=0;j<12;j++)
+		{
+			if(ady[k][j] == 1)
+			{
+				ady[k][j] = 2;	
+	//			ban=true;
+				break;
+			}
+
+
+		}
+		//if(ban == true)
+			break;
+
+				
+	}
+
+
+	imprimirtab(ady);
+
+}
